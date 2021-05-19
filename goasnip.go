bea430f3c637875827f33cd414a7bdb7dedbb36c
krishpranav <krisna.pranav@gmail.com>
2021-05-19 18:29:03 +0530

    [+] wirte Lines Functions Updated [+]

--- goasnip.go
+++ goasnip.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 type sliceVal []string
@@ -12,4 +15,18 @@ func (s sliceVal) String() string {
 		str += fmt.Sprintf("%s\n", i)
 	}
 	return str
+}
+
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("[!] Couldn't create file: %s\n", err.Error())
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintf(w, line)
+	}
+	return w.Flush()
 }
