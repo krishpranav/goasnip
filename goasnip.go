ae71103b6a6838e82e5eecc263fb6123482e30c8
krishpranav <krisna.pranav@gmail.com>
2021-05-19 18:27:13 +0530

    [+] String Value Added [+]

---
+++ goasnip.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+type sliceVal []string
+
+func (s sliceVal) String() string {
+	var str string
+	for _, i := range s {
+		str += fmt.Sprintf("%s\n", i)
+	}
+	return str
+}
